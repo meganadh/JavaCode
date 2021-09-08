ee0ce8fd0680d5cb6c00d5604e72bfbb0bfefc6a
Meganadh <E004787@cigniti.com>
2021-09-08 14:31:07 +0530

    fourth checkin24

--- Hello.java
+++ Hello.java
@@ -3,12 +3,5 @@ class Hello
   public static void main(String[] args)
   {
    System.out.println("We learnt 5 git commands");
-   System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
-System.out.println("We learnt 5 git commands");
   }
 }
